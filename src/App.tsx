69894710fdba29ae372e5140014c78841164b1e6
h-zahar <hz.rifat@gmail.com>
2024-01-12 14:13:52 +0600

    Added Features to Mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import DiscountTable from "./components/DiscountTable";
+import SpinnerSettings from "./components/SpinnerSettings";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1.5fr",
+          width: "80vw",
+        }}
+      >
+        <SpinnerSettings />
+        <DiscountTable />
+      </div>
+    </>
+  );
 }
 
 export default App;
